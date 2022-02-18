e855bfce5ed7b2da4972201ae734ed524ebf24a6
Lavi Costa <laviacex@gmail.com>
2022-02-18 18:09:51 -0300

    bug

--- script.js
+++ script.js
@@ -41,7 +41,15 @@ const transactions = [{
 
 const Transaction = {
     incomes() {
-        //somar as entradas
+        let income = 0;
+
+        transactions.forEach(transaction => {
+            if( transaction.amount > 0 ) {
+                income += transaction.amount;
+            }
+        })
+        return income;
+    
     },
     expenses() {
         //somar as saídas
@@ -53,23 +61,67 @@ const Transaction = {
 
 //substituicao das transacoes do html
 const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+
     addTransaction(transaction, index) {
         const tr = document.createElement('tr')
         tr.innerHTML = DOM.innerHTMLTransaction
+
+        DOM.transactionsContainer.appendChild(tr)
     },
-    innerHTMLTransaction() {
+    
+    innerHTMLTransaction(transaction) {
+        
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+        const amount = Utils.formatCurrency(transaction.amount)
+
         const html = `
-        <td class="description">Internet</td>
-        <td class="expense">- R$ 200,00</td>
-        <td class="date">23/01/2021</td>
+        <td class="description">${transaction.description}</td>
+        <td class="${CSSclass}">${amount}</td>
+        <td class="date">${transaction.date}</td>
         <td>
             <img src="./assets/minus.svg" alt="Remover transação">
         </td>
-
         `
 
         return html
+    },
+
+    updateBalance() {
+        document
+            .document.getElementById('incomeDisplay')
+            .innerHTML = Transaction.incomes()
+        document
+            .document.getElementById('expensesDisplay')
+            .innerHTML = Transaction.expenses()
+        document
+            .document.getElementById('totalDisplay')
+            .innerHTML = Transaction.total()
     }
 }
 
-DOM.addTransaction(transaction[0])
+//formatacao para o formato de valor/moeda
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : ""
+
+        value = String(value).replace(/\D/g, "")
+
+        value = Number(value) / 100
+         
+        value = value.toLocalString ("pt-BR", {
+            style: "currency",
+            currency : "BRL"
+        })
+
+        return signal + value
+    }
+}
+
+
+transactions.forEach(function(transaction){
+    DOM.addTransaction(transaction)
+})
+
+DOM.updateBalance()
